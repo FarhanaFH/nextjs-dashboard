c778c70bb1ae938745494ee2b9bb7076f457d616
Farhana <100227003+FarhanaFH@users.noreply.github.com>
2024-11-13 12:16:05 +0400

    fixed eslint errors

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,7 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data'; // Remove fetchLatestInvoices
 import { Suspense } from 'react';
 import {
   RevenueChartSkeleton,
@@ -13,12 +11,6 @@ import {
  
 export default async function Page() {
   // Remove `const latestInvoices = await fetchLatestInvoices()`
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
  
   return (
     <main>
